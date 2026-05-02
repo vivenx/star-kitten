0ebe2b334f464f6cfa836d6bc8268a6ff41ed089
Angelina Shumkova <shumkova1408@gmail.com>
2026-05-02 10:31:11 +0500

    player animation

---
+++ core/player.py
@@ -0,0 +1,89 @@
+import pygame
+import os
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+
+        self.sprites = {
+            'right': [],
+            'left': [],
+            'up': [],
+            'down': []
+        }
+
+        base_path = 'assets/images/sprites_player'
+
+        right_path = os.path.join(base_path, 'right')
+        for i in range(1, 6):
+            sprite = pygame.image.load(os.path.join(right_path, f'{i}_step.png')).convert_alpha()
+            self.sprites['right'].append(sprite)
+
+        left_path = os.path.join(base_path, 'left')
+        for i in range(1, 6):
+            sprite = pygame.image.load(os.path.join(left_path, f'{i}_step.png')).convert_alpha()
+            self.sprites['left'].append(sprite)
+
+        up_down_path = os.path.join(base_path, 'up_and_down')
+        for i in range(1, 6):
+            sprite = pygame.image.load(os.path.join(up_down_path, f'{i}_step.png')).convert_alpha()
+            self.sprites['down'].append(sprite)
+            self.sprites['up'].append(sprite)
+
+        self.direction = 'down'
+        self.current_frame = 0
+        self.animation_speed = 5
+        self.animation_counter = 0
+
+        self.image = self.sprites[self.direction][self.current_frame]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.speed = 5
+
+        self.is_moving = False
+
+    def update(self):
+        if self.is_moving:
+            self.animation_counter += 1
+            if self.animation_counter >= self.animation_speed:
+                self.animation_counter = 0
+                self.current_frame = (self.current_frame + 1) % len(self.sprites[self.direction])
+                self.image = self.sprites[self.direction][self.current_frame]
+        else:
+            self.current_frame = 3
+            self.direction = 'down'
+            self.image = self.sprites[self.direction][self.current_frame]
+
+    def move(self, dx, dy):
+        self.is_moving = False
+
+        if dx > 0:
+            self.direction = 'right'
+            self.rect.x += self.speed
+            self.is_moving = True
+        elif dx < 0:
+            self.direction = 'left'
+            self.rect.x -= self.speed
+            self.is_moving = True
+
+        if dy > 0:
+            self.direction = 'down'
+            self.rect.y += self.speed
+            self.is_moving = True
+        elif dy < 0:
+            self.direction = 'up'
+            self.rect.y -= self.speed
+            self.is_moving = True
+
+        screen_width, screen_height = pygame.display.get_surface().get_size()
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > screen_width:
+            self.rect.right = screen_width
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
--- scenes/game_scene.py
+++ scenes/game_scene.py
@@ -1,9 +1,17 @@
 import pygame
+from core.player import Player
 
 
 class GameScene:
     def __init__(self, game):
         self.game = game
+        self.player = None
+
+        self._create_player()
+
+    def _create_player(self):
+        screen_width, screen_height = self.game.screen.get_size()
+        self.player = Player(screen_width // 2 - 50, screen_height // 2 - 50)
 
     def handle_events(self):
         for event in pygame.event.get():
@@ -15,7 +23,25 @@ class GameScene:
                     self.game.change_scene("menu")
 
     def update(self):
-        pass
+        if self.player:
+            keys = pygame.key.get_pressed()
+            dx = 0
+            dy = 0
+
+            if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                dx = -1
+            if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                dx = 1
+            if keys[pygame.K_UP] or keys[pygame.K_w]:
+                dy = -1
+            if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+                dy = 1
+
+            self.player.move(dx, dy)
+            self.player.update()
 
     def draw(self):
         self.game.screen.fill((20, 20, 30))
+
+        if self.player:
+            self.game.screen.blit(self.player.image, self.player.rect)
